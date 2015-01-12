d7168a81f12e1de4eb5e29e6840589809b0e6686
benny0642 <benny0642@gmail.com>
2015-01-12 22:33:10 +0800

    bomb game start. project complete with javascript.

---
+++ guestbook.py
@@ -0,0 +1,24 @@
+from google.appengine.api import users
+import os
+import urllib
+
+from google.appengine.ext import ndb
+import webapp2
+import jinja2 
+import logging
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+	loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+	extensions=['jinja2.ext.autoescape'],
+	autoescape=True)
+
+class MainPage(webapp2.RequestHandler):
+	def get(self):
+		
+		template_values = {}
+		template = JINJA_ENVIRONMENT.get_template('index.html')
+		self.response.write(template.render(template_values))
+
+application = webapp2.WSGIApplication([
+ 	('/', MainPage),	
+], debug=True)
